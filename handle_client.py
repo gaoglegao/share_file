89ad70b249d4c1c705ae32a871910d7b879668f5
gaoglegao <359854738@qq.com>
2022-03-09 21:27:04 +0800

    Update handle_client.py

--- handle_client.py
+++ handle_client.py
@@ -4,8 +4,8 @@ import threading
 def task(stop_thread_flg):
     while True:
         if "run" == stop_thread_flg["status"]:
-            print("线程:",stop_thread_flg["status"])
+            print("线程更新:",stop_thread_flg["status"])
             time.sleep(2)
         else:
-            print("线程:",stop_thread_flg["status"])
+            print("线程更新:",stop_thread_flg["status"])
             break
